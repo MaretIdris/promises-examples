ca9000292c01f40566b53274846053e6d74fcb69
Maret Idris <maret@fasterlap.com>
2022-02-12 10:58:56 -0800

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import './style.css'
+import SeaTurtle from './assets/images/sea-turtle.png'
+import Pig from './assets/images/pig.png'
+
+/*Below are examples how to use images and fonts in Webpack 5.*/
+const header = document.createElement('h1')
+header.innerHTML = "I am a header... weeee!"
+header.classList.add('hello')
+
+const seaTurtleImage = new Image()
+seaTurtleImage.src = SeaTurtle
+
+const pigImage = new Image()
+pigImage.src = Pig
+
+const body = document.body
+body.appendChild(header)
+body.appendChild(seaTurtleImage)
+body.appendChild(pigImage)
+
+document.title = "Starter project"
+
+export {}
